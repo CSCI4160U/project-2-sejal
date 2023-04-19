6a9086c0bc71856a172deac8acc2d6a036aacd69
Sejal1109 <sejal.shingal2001@gmail.com>
2023-04-18 22:01:30 -0400

    Added skybox and made UI scalable

---
+++ Assets/Scripts/HealthStatusEnemy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthStatusEnemy : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public Image healthBarImage;
+
+    void Start() 
+    {
+        enemyTransform = this.transform.parent.transform.parent.transform;
+    }
+    void Update()
+    {
+
+        Vector3 eHeadPos = enemyTransform.position + Vector3.up * 2f;
+        Vector3 healthBarPosition = Camera.main.WorldToScreenPoint(eHeadPos);
+        healthBarPosition.z = 0f;
+
+        GetComponent<RectTransform>().position = healthBarPosition;
+
+
+        float healthPercent = enemyTransform.GetComponent<Enemy>().GetHealthPercent();
+        healthBarImage.fillAmount = healthPercent;
+        if (healthPercent <= 0.7)
+        {
+            healthBarImage.color = new Color(0.9333333f, 0.594713f, 0.00392154f);
+        }
+        if (healthPercent <= 0.4)
+        {
+            healthBarImage.color = Color.red;
+        }
+    }
+}
